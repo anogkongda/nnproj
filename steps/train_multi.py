6ad7bf746bd3a2d02f385ff633b4db9dd180b544
anogkongda <918063599@qq.com>
2020-07-10 11:14:42 +0800

    reorganize multi

--- steps/train_multi.py
+++ steps/train_multi.py
@@ -8,7 +8,7 @@ import time
 import yaml
 import argparse
 import numpy as np
-
+import copy
 import torch
 import torch.nn as nn
 from collections.abc import Iterable
@@ -28,7 +28,199 @@ def gen_batch(dataloader):
     for i,data in enumerate(dataloader):
         yield data
 
-def run_epoch(epoch_id, model, data_iter,loss_fn, device,semi_data_iter=None, optimizer=None, print_every=20, is_training=True, dat=False, mme=False,language_one_hot=False):
+def language_one_hot_cat(inputs, real_dataset_id, num_sup_dataset):
+    B,T,_ = inputs.shape
+    device = inputs.device
+    lids = torch.zeros((B,T,num_sup_dataset))
+    lids[:,:,real_dataset_id] = 1
+    inputs = torch.cat((inputs, lids), dim=-1)
+    return inputs.to(device)
+
+def sup_get_data(data, device):
+    inputs, input_sizes, targets, target_sizes, utt_list = data
+    targets = targets.to(device)
+    target_sizes = target_sizes.to(device)
+    inputs = inputs.to(device)
+    input_sizes = input_sizes.to(device)
+    return inputs, input_sizes,targets, target_sizes, utt_list
+
+
+
+'''
+common input: model,loss_fn, data, device, dataset_idxs
+common output: loss, out, batch_size, input_sizes, targets, target_sizes
+'''
+def normal_sup_training(model,loss_fn, data, device, dataset_idx, num_sup_dataset):
+    inputs, input_sizes, targets, target_sizes, utt_list = sup_get_data(data, device)
+    out = model(inputs, set_id=dataset_idx, mmen_grl=False)
+    out_len, batch_size, _ = out.size()
+    input_sizes = (input_sizes * out_len).long()
+    loss = loss_fn(out, targets, input_sizes, target_sizes)
+    return loss, out, batch_size, input_sizes, targets, target_sizes
+
+
+
+def dat_sup_training(model,loss_fn, data, device, dataset_idx, num_sup_dataset):
+    inputs, input_sizes, targets, target_sizes, utt_list = sup_get_data(data, device)
+    out,language_out = model(inputs, set_id = dataset_idx)
+    out_len, batch_size, _ = out.size()
+    input_sizes = (input_sizes * out_len).long()
+
+    language_target = torch.zeros(batch_size)
+    language_target = language_target.fill_(dataset_idx)
+    language_target = language_target.view(-1).long().to(device)
+    ce = nn.CrossEntropyLoss(ignore_index=-1,reduction='sum')
+    language_loss = ce(language_out, language_target)
+
+    asr_loss = loss_fn(out, targets, input_sizes, target_sizes)
+    loss = asr_loss + language_loss
+    return loss, out, batch_size, input_sizes, targets, target_sizes
+
+
+
+def dat_semi_training(model,loss_fn, data, device, dataset_idx, num_sup_dataset):
+    inputs, input_sizes, utt_list = data
+    real_dataset_id = dataset_idx - num_sup_dataset
+    out,language_out = model(inputs, set_id = real_dataset_id)
+    out_len, batch_size, _ = out.size()
+    input_sizes = (input_sizes * out_len).long()
+
+    language_target = torch.zeros(batch_size)
+    language_target = language_target.fill_(real_dataset_id)
+    language_target = language_target.view(-1).long().to(device)
+    ce = nn.CrossEntropyLoss(ignore_index=-1,reduction='sum')
+    language_loss = ce(language_out, language_target)
+    return language_loss, out, batch_size, input_sizes, 0,0
+
+
+def mme_semi_training(model,loss_fn, data, device, dataset_idx, num_sup_dataset):
+    inputs, input_sizes, utt_list = data
+    real_dataset_id = dataset_idx - num_sup_dataset
+    out = model(inputs,set_id=real_dataset_id, mmen_grl=True)
+    out_len, batch_size, _ = out.size()
+    input_sizes = (input_sizes * out_len).long()
+    prob = torch.softmax(out, dim=-1)
+    loss = 0.1 * torch.sum(torch.mul(prob,torch.log2(prob+1e-8))) # H(x)
+    return loss, out, batch_size, input_sizes,0,0
+
+def code_from_last_version():
+       # if dataset_idx < num_sup_dataset: # supervised
+            #     inputs, input_sizes, targets, target_sizes, utt_list = data
+            #     targets = targets.to(device)
+            #     target_sizes = target_sizes.to(device)
+            # else:
+            #     inputs, input_sizes, utt_list = data
+            #     real_dataset_id = dataset_idx - num_sup_dataset
+                
+            # if language_one_hot:
+            #     B,T,_ = inputs.shape
+            #     real_dataset_id = dataset_idx if dataset_idx < num_sup_dataset else (dataset_idx - num_sup_dataset)
+            #     lids = torch.zeros((B,T,num_sup_dataset))
+            #     lids[:,:,real_dataset_id] = 1
+            #     inputs = torch.cat((inputs, lids), dim=-1)
+            # inputs = inputs.to(device)
+            # input_sizes = input_sizes.to(device)
+            
+            # if maml and dataset_idx < num_sup_dataset and is_training: # only support supervised training
+            #     model_backup = copy.deepcopy(model) # gradient update on origin model.
+            #     batch_size = inputs.shape[0]
+            #     divide_index = int(batch_size/2)
+
+
+            #     inputs_tr = inputs[:divide_index].clone().to(device)
+            #     input_sizes_tr = input_sizes[:divide_index].clone().to(device)
+            #     targets_tr = targets[:divide_index].clone().to(device)
+            #     target_sizes_tr = target_sizes[:divide_index].clone().to(device)
+
+            #     fake_out = model(inputs_tr, set_id=dataset_idx, mmen_grl=False)
+            #     fake_out_len,_,_ = fake_out.size()
+            #     fake_input_sizes = (input_sizes_tr * fake_out_len).long()
+            #     fake_loss = loss_fn(fake_out, targets_tr, fake_input_sizes, target_sizes_tr)
+
+            #     # get temp model theta'
+            #     optimizer.zero_grad()
+
+            #     fake_loss.backward()
+            #     optimizer.step()
+
+            #     # use whole test batch
+            #     inputs = inputs[divide_index:]
+            #     input_sizes = input_sizes[divide_index:]
+            #     targets = targets[divide_index:]
+            #     target_sizes = target_sizes[divide_index:]
+            #     out = model(inputs, set_id=dataset_idx, mmen_grl=False)
+            #     out_len, batch_size, _ = out.size()
+            #     input_sizes = (input_sizes * out_len).long()
+            #     loss = loss_fn(out, targets, input_sizes, target_sizes)
+
+            #     optimizer.zero_grad()
+            #     loss.backward()
+
+            #     # restore theta' to theta
+            #     model.load_state_dict(model_backup.state_dict())
+            #     # theta + alpha * grad_with_theta'
+            #     optimizer.step()
+
+            # else:
+            #     if dat:
+            #         if dataset_idx < num_sup_dataset: # supervised
+            #             out,language_out = model(inputs, set_id = dataset_idx)
+            #         else:
+            #             out,language_out = model(inputs, set_id = dataset_idx-num_sup_dataset)
+            #         out_len, batch_size, _ = out.size()
+            #         input_sizes = (input_sizes * out_len).long()
+
+            #         language_target = torch.zeros(batch_size)
+
+            #         if dataset_idx < num_sup_dataset: # supervised
+            #             language_target = language_target.fill_(dataset_idx)
+            #         else:
+            #             language_target = language_target.fill_(dataset_idx - num_sup_dataset)
+            #         # for i in range(batch_size):
+            #         #     language_target[i][:input_sizes[i].item()] = dataset_idx
+                    
+            #         language_target = language_target.view(-1).long().to(device)
+            #         ce = nn.CrossEntropyLoss(ignore_index=-1,reduction='sum')
+            #         language_loss = ce(language_out, language_target)
+                    
+            #         if dataset_idx < num_sup_dataset: # supervised:
+            #             asr_loss = loss_fn(out, targets, input_sizes, target_sizes)
+            #         else:
+            #             asr_loss = 0
+            #         loss =  asr_loss+ language_loss
+            #     else: # add min max
+            #         if dataset_idx < num_sup_dataset:
+            #             out = model(inputs, set_id=dataset_idx, mmen_grl=False)
+            #             out_len, batch_size, _ = out.size()
+            #             input_sizes = (input_sizes * out_len).long()
+            #             loss = loss_fn(out, targets, input_sizes, target_sizes)
+            #         else:
+            #             if mme:
+            #                 out = model(inputs,set_id=dataset_idx-num_sup_dataset, mmen_grl=True)
+            #                 out_len, batch_size, _ = out.size()
+            #                 input_sizes = (input_sizes * out_len).long()
+            #                 prob = torch.softmax(out, dim=-1)
+            #                 loss = 0.1 * torch.sum(torch.mul(prob,torch.log2(prob+1e-8))) # H(x)
+            #             else:
+            #                 continue
+
+
+            # loss /= batch_size
+            # cur_loss += loss.item()
+            # total_loss += loss.item()
+            # prob, index = torch.max(out, dim=-1)
+            # if dataset_idx < num_sup_dataset:
+            #     batch_errs, batch_tokens = model.compute_wer(index.transpose(0,1).cpu().numpy(), input_sizes.cpu().numpy(), targets.cpu().numpy(), target_sizes.cpu().numpy())
+            # else:
+            #     batch_errs = 0
+            #     batch_tokens = 0 # do not compute wer
+
+            #if not is_training:
+            #    pdb.set_trace()
+    pass
+
+#dat=False, mme=False,language_one_hot=False, maml=False
+def run_epoch(epoch_id, model, data_iter,loss_fn, device,opts,semi_data_iter=None, optimizer=None, print_every=20, is_training=True):
     if is_training:
         model.train()
     else:
@@ -55,72 +247,29 @@ def run_epoch(epoch_id, model, data_iter,loss_fn, device,semi_data_iter=None, op
 
         try:
             data = next(curr_dataset)
-            if dataset_idx < num_sup_dataset: # supervised
-                inputs, input_sizes, targets, target_sizes, utt_list = data
-                targets = targets.to(device)
-                target_sizes = target_sizes.to(device)
-
+            supervised = True
+            if not dataset_idx < num_sup_dataset:
+                supervised = False
+            forward_func = None
+            if supervised:
+                if opts.dat_lambda > 0: # dat
+                    forward_func = dat_sup_training
+                else:
+                    forward_func = normal_sup_training
             else:
-                inputs, input_sizes, utt_list = data
-                real_dataset_id = dataset_idx - num_sup_dataset
-                
-            if language_one_hot:
-                B,T,_ = inputs.shape
-                real_dataset_id = dataset_idx if dataset_idx < num_sup_dataset else (dataset_idx - num_sup_dataset)
-                lids = torch.zeros((B,T,num_sup_dataset))
-                lids[:,:,real_dataset_id] = 1
-                inputs = torch.cat((inputs, lids), dim=-1)
-            inputs = inputs.to(device)
-            input_sizes = input_sizes.to(device)
-            
-            if dat:
-                if dataset_idx < num_sup_dataset: # supervised
-                    out,language_out = model(inputs, set_id = dataset_idx)
+                if opts.dat_lambda > 0:
+                    forward_func = dat_semi_training
+                elif opts.mme_lambda > 0:
+                    forward_func = mme_semi_training
                 else:
-                    out,language_out = model(inputs, set_id = dataset_idx-num_sup_dataset)
-                out_len, batch_size, _ = out.size()
-                input_sizes = (input_sizes * out_len).long()
-
-                language_target = torch.zeros(batch_size)
-
-                if dataset_idx < num_sup_dataset: # supervised
-                    language_target = language_target.fill_(dataset_idx)
-                else:
-                    language_target = language_target.fill_(dataset_idx - num_sup_dataset)
-                # for i in range(batch_size):
-                #     language_target[i][:input_sizes[i].item()] = dataset_idx
-                
-                language_target = language_target.view(-1).long().to(device)
-                ce = nn.CrossEntropyLoss(ignore_index=-1,reduction='sum')
-                language_loss = ce(language_out, language_target)
-                
-                if dataset_idx < num_sup_dataset: # supervised:
-                    asr_loss = loss_fn(out, targets, input_sizes, target_sizes)
-                else:
-                    asr_loss = 0
-                loss =  asr_loss+ language_loss
-            else: # add min max
-                if dataset_idx < num_sup_dataset:
-                    out = model(inputs, set_id=dataset_idx, mmen_grl=False)
-                    out_len, batch_size, _ = out.size()
-                    input_sizes = (input_sizes * out_len).long()
-                    loss = loss_fn(out, targets, input_sizes, target_sizes)
-                else:
-                    if mme:
-                        out = model(inputs,set_id=dataset_idx-num_sup_dataset, mmen_grl=True)
-                        out_len, batch_size, _ = out.size()
-                        input_sizes = (input_sizes * out_len).long()
-                        prob = torch.softmax(out, dim=-1)
-                        loss = 0.1 * torch.sum(torch.mul(prob,torch.log2(prob+1e-8))) # H(x)
-                    else:
-                        continue
-
-
+                    continue # no semi-sup training
+            loss, out, batch_size, input_sizes, targets, target_sizes = forward_func(model, loss_fn, data, device, dataset_idx, num_sup_dataset)
             loss /= batch_size
             cur_loss += loss.item()
             total_loss += loss.item()
             prob, index = torch.max(out, dim=-1)
-            if dataset_idx < num_sup_dataset:
+
+            if supervised:
                 batch_errs, batch_tokens = model.compute_wer(index.transpose(0,1).cpu().numpy(), input_sizes.cpu().numpy(), targets.cpu().numpy(), target_sizes.cpu().numpy())
             else:
                 batch_errs = 0
@@ -130,11 +279,12 @@ def run_epoch(epoch_id, model, data_iter,loss_fn, device,semi_data_iter=None, op
             total_tokens += batch_tokens
 
             if (i + 1) % print_every == 0 and is_training:
-                print('Epoch = %d, step = %d, cur_loss = %.4f, total_loss = %.4f, total_wer = %.4f' % (epoch_id, 
+                print('Epoch = %d, step = %d, cur_loss = %.4f, total_loss = %.4f, total_cer = %.4f' % (epoch_id, 
                                      i+1, cur_loss / print_every, total_loss / (i+1), total_errs / total_tokens ))
                 cur_loss = 0
         
-            if is_training:    
+        #not maml
+            if is_training and True:    # maml do not update here
                 optimizer.zero_grad()
                 loss.backward()
                 #nn.utils.clip_grad_norm_(model.parameters(), 400)
@@ -147,7 +297,9 @@ def run_epoch(epoch_id, model, data_iter,loss_fn, device,semi_data_iter=None, op
         
     average_loss = total_loss / (i+1)
     training = "Train" if is_training else "Valid"
-    print("Epoch %d %s done, total_loss: %.4f, total_wer: %.4f" % (epoch_id, training, average_loss, total_errs / total_tokens))
+    # if not is_training:
+    #     pdb.set_trace()
+    print("Epoch %d %s done, total_loss: %.4f, total_cer: %.4f" % (epoch_id, training, average_loss, total_errs / total_tokens))
     return 1-total_errs / total_tokens, average_loss
 
 class Config(object):
@@ -296,9 +448,9 @@ def main(conf):
         
         print("Start training epoch: %d, learning_rate: %.5f" % (count, learning_rate))
         
-        train_acc, loss = run_epoch(count, model, train_loader,loss_fn, device,semi_loader,optimizer=optimizer, print_every=opts.verbose_step, is_training=True, dat=dat, mme=mme,language_one_hot=opts.language_one_hot)
+        train_acc, loss = run_epoch(count, model, train_loader,loss_fn, device,opts,semi_loader,optimizer=optimizer, print_every=opts.verbose_step, is_training=True)
         loss_results.append(loss)
-        acc, dev_loss = run_epoch(count, model, dev_loader, loss_fn, device, optimizer=None, print_every=opts.verbose_step, is_training=False, dat=dat, mme=mme,language_one_hot=opts.language_one_hot)
+        acc, dev_loss = run_epoch(count, model, dev_loader, loss_fn, device,opts, optimizer=None, print_every=opts.verbose_step, is_training=False)
         print("loss on dev set is %.4f" % dev_loss)
         dev_loss_results.append(dev_loss)
         dev_cer_results.append(acc)
